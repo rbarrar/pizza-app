e7c5d78577f8495de616150cb03a2598ce8a0417
Ryan Barrar <Rbarrar17@gmail.com>
2016-05-05 16:41:14 -0400

    added slideshow to homepage

--- public/home.js
+++ public/home.js
@@ -1,3 +1,18 @@
 $(document).on("page:change", function(){
   $('.dropdown-toggle').dropdown();
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
