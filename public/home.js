aef6ae6b796334e13b92049c4422a00731cf125b
Auston Bunsen <auston.bunsen@gmail.com>
2016-05-17 23:03:47 -0400

    helpin a yung blood figure things out in dis hea hood

--- public/home.js
+++ public/home.js
@@ -14,20 +14,29 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
+  if($(".mySlides").length > 0){
     setTimeout(carousel, 2000); // Change image every 2 seconds
+  }
+    // ^^  nice use of recursion
 }
 
 $(document).ready(function() {
   $("a").click(function(event){
     event.preventDefault();
     console.log(this.href);
+    $('#content-container').html("<img src='/images/loader.gif' class='loader' /> <br/> Loading toppings...");
     $.ajax({
       url: this.href,
       success: function(){
         var jsonObject = arguments[0];
-        console.log(jsonObject);
-        var html = $.parseJSON(jsonObject);
-        console.log(html.Toppings);
+        // console.log(jsonObject);
+        var response = $.parseJSON(jsonObject);
+        var toppingsHTML = "";
+        $.each(response.Toppings, function(i, topping){
+          toppingsHTML += "<p>"+Object.keys(topping)[0]+"</p>"; // this is just one way to do this...
+        });
+        $("#content-container").html(toppingsHTML);
+        // loop through toppings and create an LI for
       }
     })
   });
